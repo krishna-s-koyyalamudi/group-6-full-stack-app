2627b5dd7fb08d177bd9c4cfd2b8b71eb08d4514
swaroop reddy <reddy.swaroop2016@gmail.com>
2021-03-03 01:52:18 -0600

    Create maven-archetype-quickstart project 1.4

---
+++ hunt6/src/main/java/edu/nwmissouri/group6/hunt6/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.group6.hunt6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
