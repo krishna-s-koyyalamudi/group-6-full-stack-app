c15279a70e9eecbae79c37156826afbf5667fa1e
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-03-10 00:30:59 -0600

    remove project

---
+++ hunt6/src/main/java/edu/nwmissouri/group6/hunt6/service/Hunt6Service.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.group6.hunt6.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Hunt6Service {
+
+	@Autowired
+	private HuntRepository repo;
+	
+	
+}
