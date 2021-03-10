6eae5decb0c5a93fec34b709d1eddf68bdb3b0d6
swaroop reddy <reddy.swaroop2016@gmail.com>
2021-03-10 01:51:28 -0600

    Add structured project

---
+++ hunt6/src/main/java/edu/nwmsu/group6/hunt6/Hunt6Application.java
@@ -0,0 +1,13 @@
+package edu.nwmsu.group6.hunt6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hunt6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hunt6Application.class, args);
+	}
+
+}
