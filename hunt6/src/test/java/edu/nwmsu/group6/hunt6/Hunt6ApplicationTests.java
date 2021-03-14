f065339a77fdea47dfa563c1d086bd60e27e8973
swaroop reddy <reddy.swaroop2016@gmail.com>
2021-03-13 22:39:23 -0600

    resolved project build

---
+++ hunt6/src/test/java/edu/nwmsu/group6/hunt6/Hunt6ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmsu.group6.hunt6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hunt6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
