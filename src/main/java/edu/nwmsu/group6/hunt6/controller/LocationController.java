6421314930ca8933aafdd1887e3bf7de94736f67
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-03-17 03:05:15 -0500

    Complete CRUDL operations

--- src/main/java/edu/nwmsu/group6/hunt6/controller/LocationController.java
+++ src/main/java/edu/nwmsu/group6/hunt6/controller/LocationController.java
@@ -2,36 +2,59 @@ package edu.nwmsu.group6.hunt6.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.nwmsu.group6.hunt6.entity.Location;
+import edu.nwmsu.group6.hunt6.exception.ResourceNotFoundException;
 import edu.nwmsu.group6.hunt6.repository.LocationRepository;
 
 @RestController
 public class LocationController {
 
 	@Resource
-    private LocationRepository loc;
+	private LocationRepository locationRepository;
+
+	@GetMapping("/locations")
+	public Page<Location> getLocations(Pageable pageable) {
+		return locationRepository.findAll(pageable);
+	}
 	
-	
+	@GetMapping("/location")
+	public Page<Location> getLocation(int locationid) {
+		Location location=locationRepository.findById(locationid).get();
+		return (Page<Location>) location;
+	}
+
+	@PostMapping("/locations")
 	public void createLocation(Location l) {
-		loc.save(l);
+		locationRepository.save(l);
 	}
-	
-	public Location getLocation(int id) {
-		Location lg=loc.findById(id).get();
-		return lg;
+
+	@PutMapping("/locations/{locationId}")
+	public Location updateLocation(@PathVariable int locationId, @Validated @RequestBody Location locationRequest) {
+		return locationRepository.findById(locationId).map(location -> {
+			location.setLocation_name(locationRequest.getLocation_name());
+			location.setLatitude(locationRequest.getLatitude());
+			location.setLongitude(locationRequest.getLongitude());
+			return locationRepository.save(location);
+		}).orElseThrow(() -> new ResourceNotFoundException("Location not found with id " + locationId));
 	}
-	
-	public void updateLocation(int id,double lat, double lon) {
-		Location loca = loc.findById(id).get();
-		loca.setLatitude(lat);
-		loca.setLongitude(lon);
-		loc.save(loca);
+
+	@DeleteMapping("/locations/{locationId}")
+	public ResponseEntity<?> deleteLocation(@PathVariable int locationId) {
+		return locationRepository.findById(locationId).map(location -> {
+			locationRepository.delete(location);
+			return ResponseEntity.ok().build();
+		}).orElseThrow(() -> new ResourceNotFoundException("Location not found with id " + locationId));
 	}
-	
-	public void deleteLocation(int id) {
-		loc.deleteById(id);
-	}
-	
 }
